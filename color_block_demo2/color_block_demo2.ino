a8eee42a5eba17765abc3fe9d17434f10724aa85
Yang Liu <yxl1450@case.edu>
2018-01-30 21:45:44 -0500

    demo2 speaker

--- color_block_demo2/color_block_demo2.ino
+++ color_block_demo2/color_block_demo2.ino
@@ -60,19 +60,19 @@ void setup() {
     
     // 3.speaker
     // iterate over the notes of the melody:
-//    for (int thisNote = 0; thisNote < 8; thisNote++) {
-//        // to calculate the note duration, take one second
-//        // divided by the note type.
-//        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-//        int noteDuration = 1000 / noteDurations[thisNote];
-//        tone(SPEAKER, melody[thisNote], noteDuration);
-//        // to distinguish the notes, set a minimum time between them.
-//        // the note's duration + 30% seems to work well:
-//        int pauseBetweenNotes = noteDuration * 1.30;
-//        delay(pauseBetweenNotes);
-//        // stop the tone playing:
-//        noTone(SPEAKER);
-//    }
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+        // to calculate the note duration, take one second
+        // divided by the note type.
+        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+        int noteDuration = 1000 / noteDurations[thisNote];
+        tone(SPEAKER, melody[thisNote], noteDuration);
+        // to distinguish the notes, set a minimum time between them.
+        // the note's duration + 30% seems to work well:
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        // stop the tone playing:
+        noTone(SPEAKER);
+    }
 
     // 4.vibration motor
     // fade in from min to max in increments of 5 points:
