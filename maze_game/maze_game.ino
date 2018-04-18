fdd1d486c2eab2c9b312d7c29b6cae0d9bce0d13
yangliu28 <yxl1450@case.edu>
2018-04-18 18:35:29 -0400

    add mpu6050lib

--- maze_game/maze_game.ino
+++ maze_game/maze_game.ino
@@ -1,35 +1,34 @@
 // This is the 3D maze game for the color block 2.0
 
 #include "neopixel_leds.h"
-#include <XBee.h>
-#include <SoftwareSerial.h>
 #include "pitches.h"
-#include "I2Cdev.h"
-#include "MPU6050.h"
 #include "Wire.h"
+#include "mpu6050lib.h"
 
 // hardware connections
 #define SPEAKER 3
 #define VIBR_MOTOR 5
 
 NeopixelLeds neopixelleds;
-MPU6050 accelgyro;
+
+
+
+
 
 unsigned long timer_last, timer_now;
 const int PATTERN_PERIOD = 20000;
 
 void setup() {
+    Serial.begin(9600);  // print test message
     // mpu6050
-    Serial.begin(9600);  // print out test message
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    Wire.begin();
+    Wire.begin();  // join I2C bus
     // initialize device
     Serial.println("Initializing I2C devices...");
-    accelgyro.initialize();
+    mpu6050.initialize();
     // verify connection
     Serial.println("Testing device connections...");
-    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
+    Serial.println(mpu6050.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  
     // vibration motor
     // fade in from min to max in increments of 5 points:
     for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
@@ -47,7 +46,7 @@ void setup() {
 
 void loop() {
     // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    mpu6050.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
     // display tab-separated accel/gyro x/y/z values
     Serial.print("a/g:\t");
     Serial.print(ax); Serial.print("\t");
---
+++ maze_game/mpu6050lib.c
@@ -0,0 +1,5 @@
+#include "Arduino.h"
+#include "mpu6050.h"
+
+
+
